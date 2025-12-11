aa6fb1c653b0de0d292468c42cbac680af6f75b9
JAuxance <auxance.jourdan@proton.me>
2025-12-11 17:57:39 +0100

    refactor(sort): streamline variable usage in insertion_sort_list function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,44 +7,39 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
-	listint_t *node;
-	listint_t *prev_node;
-	listint_t *next_node;
+	listint_t *current, *to_insert, *prev_node, *next_node;
 
-	if (!list || !*list || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
 
 	current = (*list)->next;
 
 	while (current)
 	{
-		node = current;
-		while (node->prev && node->n < node->prev->n)
+		to_insert = current;
+		current = current->next;
+
+		while (to_insert->prev && to_insert->n < to_insert->prev->n)
 		{
-			prev_node = node->prev;
-			next_node = node->next;
+			prev_node = to_insert->prev;
+			next_node = to_insert->next;
 
-			node->prev = prev_node->prev;
-			node->next = prev_node;
 
-			prev_node->prev = node;
+			to_insert->prev = prev_node->prev;
+			to_insert->next = prev_node;
+
+			prev_node->prev = to_insert;
 			prev_node->next = next_node;
 
-			if (node->prev)
-				node->prev->next = node;
+			if (to_insert->prev)
+				to_insert->prev->next = to_insert;
 			else
-				*list = node;
+				*list = to_insert;
 
 			if (next_node)
 				next_node->prev = prev_node;
 
-			if (node->prev)
-				node = node->prev;
-			else
-				break;
-
-			current = node->next;
+			print_list(*list);
 		}
 	}
 }
